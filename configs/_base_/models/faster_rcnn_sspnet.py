0a3621cb1d438000a85db0970712a783932828ed
Prakash Toppo <iit2019119@iiita.ac.in>
2022-03-23 17:16:37 +0530

    change

--- configs/_base_/models/faster_rcnn_sspnet.py
+++ configs/_base_/models/faster_rcnn_sspnet.py
@@ -1,5 +1,5 @@
 model = dict(
-    type='FasterSSPNet',
+    type='MaskRCNN',
     pretrained='torchvision://resnet50',
     backbone=dict(
         type='ResNet',
@@ -11,7 +11,7 @@ model = dict(
         norm_eval=True,
         style='pytorch'),
     neck=dict(
-        type='SSFPN',
+        type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
         num_outs=5),
